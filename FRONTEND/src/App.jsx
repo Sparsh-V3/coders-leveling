31d9d81ffd0d49340bb3579947562c487c5f009f
unknown <vardhansparsh46@gmail.com>
2025-03-01 19:36:21 +0530

    Committed by Sparsh

--- FRONTEND/src/App.jsx
+++ FRONTEND/src/App.jsx
@@ -2,12 +2,9 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/home/Home";
 import Signup from "./pages/signup/Signup";
 import Navbar from "./components/navbar/Navbar";
-<<<<<<< HEAD
 import Login from "./pages/login/Login";
 import Dashboard from "./pages/dashboard/Dashboard";
-=======
 import Achievements from "./pages/achievements/Achievements";
->>>>>>> 3bdfe61c75d7360c5d06cace65767acc270fc6a9
 
 function App() {
   return (
@@ -16,13 +13,9 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/signup" element={<Signup />} />
-<<<<<<< HEAD
         <Route path="/login" element={<Login />} />
         <Route path="/dashboard" element={<Dashboard />} />
-=======
         <Route path="/achivements" element={<Achievements />} />
-        
->>>>>>> 3bdfe61c75d7360c5d06cace65767acc270fc6a9
       </Routes>
     </Router>
   );
